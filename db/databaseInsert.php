3f8d2678a354e284f639ab417b82c2ed2d8a502d
dawid <dawide.wilune@gmail.com>
2023-04-13 13:30:34 +0200

    creazione funzione registration

--- db/databaseInsert.php
+++ db/databaseInsert.php
@@ -2,5 +2,19 @@
     require ( "connectDB.php");
     $connessione = connectDB();
 
-    re
+    function registration($nome,$cognome,$mail,$psw,$telefono,$gender)
+    {
+        global $connessione;
+        #$sql = "SELECT * FROM utenti where email = '$email' and  passwd='$psw'";
+        $insertInto = "INSERT INTO utenti (nome,cognome,email,numero,gender,,passw) VALUES ('$nome', '$cognome', '$mail', '$telefono', '$gender','$psw');";
+        $result = $connessione->query($insertInto) or die("fail");
+        if ($result=== TRUE) 
+        {
+            return true;
+        } else 
+        {
+            echo "(/db/databaseInsert.php/registration/else)Error: " . $insertInto . "<br>" . $connessione->error;
+            return false;
+        }
+    }
 ?>
